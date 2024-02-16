eac3e519083d51f186df83957eb6aec2265c0c8d
salmanMohammed2001 <salmannisthar123@gmail.com>
2024-02-16 22:35:42 +0530

    ReviewController  createReview finalize

--- src/controller/ReviewController.ts
+++ src/controller/ReviewController.ts
@@ -13,3 +13,32 @@ export const getAllReview =async (req:any,res:any)=>{
         res.status(500).json({success:false,message:"not found"})
     }
 }
+
+export const createReview =async (req:any,res:any)=>{
+
+    if(!req.body.doctor){
+        req.body.doctor=req.params.doctorId
+    }
+
+    if(!req.body.user){
+        req.body.user=req.params.userId
+    }
+
+    try{
+
+        const newReview=new ReviewSchema(req.body)
+
+
+        const saveReview=await newReview.save();
+
+        await DoctorSchema.findByIdAndUpdate(req.body.doctor,{
+            $push:{reviews:saveReview._id}
+        })
+
+        res.status(200).json({success:true,message:"Review Submit",data:saveReview})
+
+
+    }catch (error){
+        res.status(500).json({success:false,message:"not found"})
+    }
+}
